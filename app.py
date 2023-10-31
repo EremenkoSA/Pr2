ca888ba33ad37d9faf936bfeeb5b177859863cc0
EremenkoSA <135714875+EremenkoSA@users.noreply.github.com>
2023-10-31 23:06:32 +0300

    update 3
    Бд добавлены все таблицы, возможность удаления, добавления, изменения.

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template, url_for, request, redirect
-from datetime import datetime
+import datetime
 from flask_sqlalchemy import SQLAlchemy
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
+import qrcode
+from fpdf import FPDF
 
 app = Flask(__name__)
-engine = sqlalchemy.create_engine("mariadb+mariadbconnector://root:1111@127.0.0.1:3306/pract")
 app.config['SQLALCHEMY_DATABASE_URI'] = "mariadb+mariadbconnector://root:1111@127.0.0.1:3306/pract"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+current_date = datetime.date.today()
 
 class Event(db.Model):
     id = db.Column(db.INT, primary_key=True)
@@ -16,25 +19,108 @@ class Event(db.Model):
     date_start = db.Column(db.DATE, nullable=False)
     date_end = db.Column(db.DATE, nullable=False)
     link = db.Column(db.VARCHAR(100), nullable=False)
+    #ans = db.relationship('Answers', backref='author', lazy='dynamic')
+    #answers = db.relationship("Answers", back_populates="event")
     def __repr__(self):
         return '<Event %r>' % self.id
  
-class Qustions(db.Model):    
-    q_id = db.Column(db.INT, primary_key=True)
-    text = db.Column(db.VARCHAR(100), nullable=False)
+class Answers(db.Model):
+    id = db.Column(db.INT, primary_key=True)
+    event_id = db.Column(db.INT, db.ForeignKey('event.id'), nullable=False)
+    questions_id = db.Column(db.VARCHAR(100), db.ForeignKey('qustions.id'), nullable=False)
+    grade = db.Column(db.INT, nullable=False)
+    comment = db.Column(db.VARCHAR(100))
+    # event = db.relationship("Event", back_populates="answers")
+    # questions = db.relationship("Questions", back_populates="answers")
     def __repr__(self):
-        return '<Qustions %r>' % self.q_id
+        return '<Answers %r>' % self.id
+    
+class Qustions(db.Model):    
+    id = db.Column(db.INT, primary_key=True)
+    text = db.Column(db.VARCHAR(100), nullable=False)
+    #ans2 = db.relationship('Answers', backref='author', lazy='dynamic')
+    #answers2 = db.relationship("Answers", back_populates="questions")
+    def __repr__(self):
+        return '<Qustions %r>' % self.id
 
-@app.route('/')
-def index():
-    event = Event.query.order_by(Event.date_start).all()
-    return render_template("index.html", event=event)
 
-@app.route('/report')
-def report():
-    return render_template("report.html")
 
-@app.route('/create', methods=['POST', 'GET'])
+
+
+
+
+#Создание ответа на определённый вопрос 
+@app.route('/create_a', methods=['POST', 'GET'])
+def created_answers():
+    if request.method == "POST":
+        event_id = request.form['event_id']
+        questions_id = request.form['questions_id']
+        grade = request.form['grade']
+        comment = request.form['comment']
+
+        answers = Answers(event_id=event_id, questions_id=questions_id, grade=grade, comment=comment)
+        try:
+            db.session.add(answers)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Ошибка при довавлении"
+    else:
+        return render_template("created_answers.html")
+
+
+#Cписок ответов
+@app.route('/all_a')
+def all_answer():
+    all_answers = Answers.query.all()
+    return render_template("all_answer.html", all_answers=all_answers)
+
+
+#Подробности про ответ
+@app.route('/a/<int:id>')
+def a_view(id):
+    view_a = Answers.query.get(id)
+    return render_template("view_answer.html", view_a=view_a)
+
+#Удаление ответа
+@app.route('/a/<int:id>/del')
+def a_delete(id):
+    delete_a = Answers.query.get_or_404(id)
+
+    try:
+        db.session.delete(delete_a)
+        db.session.commit()
+        return redirect('/all_a')
+    except:
+        return "Приудалении произошла ошибка"
+
+
+#Изменение ответа
+@app.route('/a/<int:id>/update', methods=['POST', 'GET'])
+def a_update(id):
+    update = Answers.query.get(id)
+    if request.method == "POST":
+        update.event_id = request.form['event_id']
+        update.questions_id = request.form['questions_id']
+        update.grade = request.form['grade']
+        update.comment = request.form['comment']
+
+        try:
+            db.session.commit()
+            return redirect('/all_a')
+        except:
+            return "Ошибка при довавлении"
+    else:
+        return render_template("update_a.html", update=update)    
+
+
+
+
+#ВОПРОСЫ
+#ВОПРОСЫ
+#ВОПРОСЫ
+#Создание вопроса
+@app.route('/create_q', methods=['POST', 'GET'])
 def created_question():
     if request.method == "POST":
         text = request.form['text']
@@ -43,14 +129,72 @@ def created_question():
         try:
             db.session.add(questions)
             db.session.commit()
-            return redirect('/')
+            return redirect('/all_q')
         except:
             return "Ошибка при довавлении"
     else:
         return render_template("created_question.html")
 
 
-@app.route('/survey', methods=['POST', 'GET'])
+#Cписок вопросов
+@app.route('/all_q')
+def all_question():
+    all_questions = Qustions.query.all()
+    return render_template("all_question.html", all_questions=all_questions)
+
+
+#Подробности про вопрос
+@app.route('/q/<int:id>')
+def q_view(id):
+    view_q = Qustions.query.get(id)
+    return render_template("view_question.html", view_q=view_q)
+
+
+#Удаление вопроса
+@app.route('/q/<int:id>/del')
+def q_delete(id):
+    delete_q = Qustions.query.get_or_404(id)
+
+    try:
+        db.session.delete(delete_q)
+        db.session.commit()
+        return redirect('/all_q')
+    except:
+        return "Приудалении произошла ошибка"
+
+
+#Изменение вопроса
+@app.route('/q/<int:id>/update', methods=['POST', 'GET'])
+def q_update(id):
+    update = Qustions.query.get(id)
+    if request.method == "POST":
+        update.text = request.form['text']
+
+        try:
+            db.session.commit()
+            return redirect('/all_q')
+        except:
+            return "Ошибка при довавлении"
+    else:
+        return render_template("update_q.html", update=update)    
+
+
+
+
+
+
+
+
+
+
+
+#ИВЕНТЫ
+#ИВЕНТЫ
+#ИВЕНТЫ
+#Создание ивента заносим данные в бд и создаём qrcode сохраняем в отдельную папку
+#Назнание пдфки с qrcode будет id ивента + .pdf 
+#Будем просматривать прошедшие мероприятия и удалять qrcode 
+@app.route('/create_e', methods=['POST', 'GET'])
 def survey():
     #return render_template("survey.html")
     if request.method == "POST":
@@ -58,8 +202,6 @@ def survey():
         date_start = request.form['date_start']
         date_end = request.form['date_end']
         link = request.form['link']
-        
-        
         event = Event(name=name, date_start=date_start, date_end=date_end, link=link)
         try:
             db.session.add(event)
@@ -68,7 +210,59 @@ def survey():
         except:
             return "Ошибка при довавлении"
     else:
-        return render_template("survey.html")
+        return render_template("created_event.html")
+
+
+#Главная страница С кнопками Главная, Добавить опрос, Помощь, Выйти
+#Добавить - опрос пока кидает на добавление вопроса
+#Помощь - нужно написать инструкцию для изпользования сайти
+#Выйти - будем логинится пока заглушка  
+@app.route('/')
+def index():
+    event = Event.query.filter(Event.date_start > current_date).all()
+    return render_template("index.html", event=event)
+
+
+@app.route('/event/<int:id>')
+def event_view(id):
+    view = Event.query.get(id)
+    return render_template("view.html", view=view)
+
+
+#Удаление ивента
+@app.route('/event/<int:id>/del')
+def event_delete(id):
+    delete_event = Event.query.get_or_404(id)
+
+    try:
+        db.session.delete(delete_event)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "Приудалении произошла ошибка"
+
+
+#Изменение ивента
+@app.route('/event/<int:id>/update', methods=['POST', 'GET'])
+def event_update(id):
+    update = Event.query.get(id)
+    if request.method == "POST":
+        update.name = request.form['name']
+        update.date_start = request.form['date_start']
+        update.date_end = request.form['date_end']
+        update.link = request.form['link']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Ошибка при довавлении"
+    else:
+        return render_template("update_event.html", update=update)    
     
+
+
+
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True)    
--- qrdsscode.py
+++ qrdsscode.py
@@ -3,8 +3,8 @@ from fpdf import FPDF
 
 pdf = FPDF()
 pdf.add_page()
-value = input('Ссылка: ')
-value2 = input('Название qrcode: ')
+value = ('123')
+value2 = ('id')
 img = qrcode.make(value)
 pdf.image(img.get_image(), x=50, y=50)
-pdf.output(value2) 
+pdf.output(value2+'.pdf') 
